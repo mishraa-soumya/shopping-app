9651ee710e635089b3db0d37870a3a273daff34d
Soumyaa Mishra <smishra@catchpoint.com>
2019-02-18 22:25:00 +0530

    updated with changes

--- server/app.js
+++ server/app.js
@@ -15,20 +15,7 @@ app.use(Express.static('./static'));
 app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
-    console.log('app started');
     res.end('app started');
-    // const app = ReactDOMServer.renderToString(<App />);
-    // // const indexFile = path.resolve('./index.html');
-    // // fs.readFile(indexFile, 'utf8', (err, data) => {
-    // //     if (err) {
-    // //         console.error('Something went wrong:', err);
-    // //         return res.status(500).send('Oops, better luck next time!');
-    // //     }
-
-    // //     return res.send(
-    // //         data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
-    // //     );
-    // // });
 });
 
 //create a http server
