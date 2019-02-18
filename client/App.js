2bc84be3e5b7990e34a494257e8788d0f5db29bf
Soumyaa Mishra <smishra@catchpoint.com>
2019-02-18 22:29:31 +0530

    Update App.js

--- client/App.js
+++ client/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 class App extends Component {
   
-  handleSubmit() {
-    e.preventDefault();
-  };
-
   render() {
     return (
       <div className="App">
